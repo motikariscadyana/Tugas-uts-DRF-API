1e4fc1d0af737fffb6ea230e5b26c8114b77db21
Motik Arisca Dyana <motikariscadyana@gmail.com>
2024-05-11 10:10:53 +0700

    edit urls.py

--- tokoku/baju/models.py
+++ tokoku/baju/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
-    name = models.CharField(max_lengt=150)
+    name = models.CharField(max_length=150)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,decimal_places=2)
 
--- tokoku/tokoku/urls.py
+++ tokoku/tokoku/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from baju.views import CategoryViewSet, ProductViewSet, OrderViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path('api/', include(router.urls)),
 ]
