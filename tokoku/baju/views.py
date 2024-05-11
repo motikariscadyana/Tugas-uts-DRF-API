cc3fb11c136905d8bd644b8687539a2dfbbd0498
Motik Arisca Dyana <motikariscadyana@gmail.com>
2024-05-11 09:53:01 +0700

    edit views.py

--- tokoku/baju/views.py
+++ tokoku/baju/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Product, Order
+from .serializers import CategorySerializer, ProductSreializer, OrderSreializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+        queryset = Product.objects.all()
+        serializer_class = ProductSreializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+        queryset = Order.objects.all()
+        serializer_class = OrderSreializer
