cf538df6119bfe937a6ab3ff7a11375eba9b7f76
Motik Arisca Dyana <motikariscadyana@gmail.com>
2024-05-15 10:13:18 +0700

    melakukan perubahan pada product

--- tokoku/baju/models.py
+++ tokoku/baju/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
+    jenis = models.CharField(max_length=150)
 
     def __str__(self):
-        return self.name
+        return self.jenis
     
 class Product(models.Model):
-    name = models.CharField(max_length=150)
+    ukuran = models.CharField(max_length=150)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    harga = models.DecimalField(max_digits=10,decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return self.ukuran 
     
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -20,4 +20,4 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f"{self.quantity} x {self.product.name}"
+        return f"{self.quantity} x {self.product.ukuran}"
