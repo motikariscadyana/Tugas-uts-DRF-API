9a3ca6741340b11b4443191ba105d31d6f40b45d
Motik Arisca Dyana <motikariscadyana@gmail.com>
2024-07-07 10:51:29 +0700

    menambah router pembeli di urls

--- tokoku/baju/urls.py
+++ tokoku/baju/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import KategoriViewSet, MerekViewSet, PakaianViewSet
+from .views import KategoriViewSet, MerekViewSet, PakaianViewSet, PembeliViewSet
 
 router = DefaultRouter()
 router.register(r'kategori', KategoriViewSet)
 router.register(r'merek', MerekViewSet)
 router.register(r'pakaian', PakaianViewSet)
+router.register(r'Pembeli', PembeliViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
